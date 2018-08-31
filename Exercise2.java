6635ea1b063968523cd3dd1ab43d4575f38ece7e
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2018-08-31 16:25:13 +0200

    Exercise2.1

---
+++ Exercise2.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.*;
+public class Exercise2{
+
+    volatile public long counter = 0;
+    
+    private class HelloThread implements Runnable{
+        private Exercise2 parent = null;
+
+        public HelloThread(Exercise2 p){
+            this.parent = p;
+        }
+    
+        public void run(){
+            for(int i = 0; i < 1000000; i++){
+                parent.increment();
+            }
+        }
+
+    }
+
+    private void increment(){
+        counter++;
+    }
+
+    public Exercise2(){
+        Thread[] threads = new Thread[2];
+        for(int i = 0; i < threads.length; i++){
+            Thread thread = new Thread(new HelloThread(this));
+            threads[i] = thread;
+            thread.start();
+        }
+
+        try{
+            for(int i = 0; i < threads.length; i++){
+                threads[i].join();
+            }
+        }catch(Exception e){
+        }
+        
+        System.out.println(counter);
+    }
+    public static void main(String[] args) {
+        new Exercise2();
+    }
+}
