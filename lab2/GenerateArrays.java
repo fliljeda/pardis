fb04f136ba30faee70e8b6192e4f8698615aa381
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2018-09-12 10:38:23 +0200

    generate arrays class

--- lab2/GenerateArrays.java
+++ lab2/GenerateArrays.java
@@ -1,19 +1,38 @@
+import java.util.*;
 public class GenerateArrays{
     public enum Mode{RANDOM, REVERSE, SORTED}
 
-    public GenerateArrays(){
+    public GenerateArrays(boolean test){
+        if(test){
+            System.out.println(generateArray(50));
+            System.out.println(generateArray(50, Mode.REVERSE));
+            System.out.println(generateArray(50, Mode.SORTED));
+        }
 
     }
 
-    public int[] generateArray(int size){
+    public ArrayList<Integer> generateArray(int size){
         return generateArray(size, Mode.RANDOM);
     }
 
-    public int[] generateArray(int size, Mode mode){
-        return null;
+    public ArrayList<Integer> generateArray(int size, Mode mode){
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        if(mode == Mode.RANDOM){
+            Random rand = new Random();
+            for(int i = 0; i < size; i++){
+                list.add(rand.nextInt(size));
+            }
+            return list;
+        }else{
+            for(int i = 0; i < size; i++){
+                int num = (mode == Mode.REVERSE) ? size - i : i;
+                list.add(num);
+            }
+            return list;
+        }
     }
 
     public static void main(String[] args){
-        new GenerateArrays();
+        new GenerateArrays(true);
     }
 }
