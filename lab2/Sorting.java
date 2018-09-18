544be6a42598aaa9f41cb349c8c67394fe8951e1
Filipjansson95 <filipjansson0@gmail.com>
2018-09-18 14:48:34 +0200

    Better memory management

--- lab2/Sorting.java
+++ lab2/Sorting.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -6,40 +7,57 @@ import java.util.concurrent.Executors;
 
 public class Sorting{
 
-    public static ArrayList<Integer> mergeSortSeq(ArrayList<Integer> list){
-        if (list.size() == 1){return list;}
-        int middle = list.size()/2;
-        ArrayList<Integer> firstHalf = new ArrayList<Integer>(middle+1);
-        ArrayList<Integer> secondHalf = new ArrayList<Integer>(middle+1);
-        for (int i = 0; i < middle; i++ ){
-            firstHalf.add(list.get(i));
+    public static void mergeSortSeq(Integer[] list, int start, int end, Integer[] returnList){
+        int size = end - start + 1;
+        //System.out.println(size);
+        if (size == 1){
+           // System.out.println("START:" + start);
+            returnList[start] = list[start];
+            return;
         }
-        for(int i = middle; i < list.size(); i++){
-            secondHalf.add(list.get(i));
-        }
-        ArrayList<Integer> firstSorted = mergeSortSeq(firstHalf);
-        ArrayList<Integer> secondSorted = mergeSortSeq(secondHalf);
-        ArrayList retList = new ArrayList();
+        int middle = start + (size-1)/2;
+      
+        mergeSortSeq(list, start, middle, returnList);
+        mergeSortSeq(list, middle+1, end, returnList);
         
-        while(firstSorted.size() > 0 && secondSorted.size() > 0){
-            if (firstSorted.get(0) > secondSorted.get(0)){
-                retList.add(secondSorted.get(0));
-                secondSorted.remove(0);
+        int firstIndex = start;
+        int sndIndex = middle+1;
+        int writeIndex = start; 
+        
+        while(firstIndex != middle+1 && sndIndex !=end+1){
+            if (returnList[firstIndex] > returnList[sndIndex]){
+                list[writeIndex] = returnList[sndIndex];
+                sndIndex++;
+                writeIndex++;
             }
             else{
-                retList.add(firstSorted.get(0));
-                firstSorted.remove(0);
+                list[writeIndex] = returnList[firstIndex];
+                firstIndex++;
+                writeIndex++;
             }
         }
 
-        for(Integer v : firstSorted){
-            retList.add(v);
+        for(int i = firstIndex; i < middle+1; i++){
+            list[writeIndex] = returnList[firstIndex];
+            writeIndex++;
+            firstIndex++;
         }
-        for(Integer v : secondSorted){
-            retList.add(v);
+        for(int i = sndIndex; i < end+1; i++){
+            list[writeIndex] = returnList[sndIndex];
+            writeIndex++;
+            sndIndex++;
+        }
+        for(int i = start; i < end+1; i++){
+            returnList[i] = list[i];
         }
 
-        return retList;
+
+        //System.out.print("Start: "+ start +" End: " + end +  "[" );
+       // for(int i = 0; i<returnList.length; i++){
+           // System.out.print(" " + returnList[i] + ",");
+       // }
+       // System.out.println("]");
+        
     }
    
 
@@ -57,11 +75,28 @@ public class Sorting{
         }
         GenerateArrays g = new GenerateArrays(false);
         ArrayList<Integer> listToSort = g.generateArray(size);
+
+        Integer[] arrToSort = new Integer[listToSort.size()];
+        listToSort.toArray(arrToSort);
+        Integer[] retArr = new Integer[listToSort.size()];
+        
         //System.out.println("Unsorted: " + listToSort);
-        //System.out.println("Sorted: " + Sorting.mergeSortSeq(listToSort));
-        Sorting.mergeSortSeq(listToSort);
+        ArrayList<Integer> l =  new ArrayList<Integer>(listToSort.size());
+        Sorting.mergeSortSeq(arrToSort, 0 , listToSort.size() - 1, retArr);
+        for (int i = 0; i < retArr.length; i++){
+            l.add(retArr[i]);
+        }
+        // System.out.println("Sorted: "+ l);
 
-
+        // // For validation
+        // Integer[] sorted = new Integer[listToSort.size()];
+        // listToSort.toArray(sorted);
+        // Arrays.sort(sorted);
+        // System.out.print("Arrays.sort = [ ");
+        // for(int i = 0; i<sorted.length;i++){
+        //     System.out.print(" "+ sorted[i]+ ",");
+        // }
+        // System.out.println("]");
 
 
     }
